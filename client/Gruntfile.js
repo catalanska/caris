293f6760a3aa5ce62bedec156ea8b01fb6241e7b
Javier Jiménez <javier@digitalorigin.com>
2015-03-15 21:08:06 +0100

    client side added

---
+++ client/Gruntfile.js
@@ -0,0 +1,115 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        clean: {
+            src: ["dist", '.tmp'],
+            server: '.tmp'
+        },
+
+        // Parses html and finds expected cssmin and ngmin
+        useminPrepare: {
+            html: ['*.html']
+        },
+
+        concat: {},
+        copy: {
+            html_files : {
+                expand: true,
+                src: ['*.html'],
+                dest: 'dist/'
+            },
+            images : {
+                expand: true,
+                src: ['images/final/**'],
+                dest: 'dist/'
+            },
+            fonts : {
+                expand: true,
+                src: ['fonts/**'],
+                dest: 'dist/'
+            }
+        },
+
+        // Replace references to scripts, css or images. Used together with rev, we point change references to assets with hash in name
+        usemin: {
+            html: ['dist/*.html'],
+            css: ['dist/{,*/}*.css']
+        },
+
+        // Add a hash in assets filenames so browser will not used cached versions if file is modified
+        rev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 8
+            },
+            assets: {
+                files: {
+                    src: [
+                        'dist/css/*.css',
+                        'dist/js/*.js'
+                    ]
+                }
+            }
+        },
+
+
+        // Uglify files so they are harder to parse
+        uglify: {
+            options: {
+                report: 'min',
+                mangle: {except: ['angular', 'ui.router', 'ngAnimate', 'CarisApp']},
+                beautify: {
+                  width: 80,
+                  beautify: false
+                }
+            }
+
+        },
+
+        html2js: {
+            main: {
+              src: ['views/*.html'],
+              dest: 'js/views.js'
+            }
+        },
+
+        ngAnnotate: {}
+
+
+    });
+
+    grunt.loadNpmTasks('grunt-ng-annotate');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-rev');
+    grunt.loadNpmTasks('grunt-usemin');
+    //Html 2 js
+    grunt.loadNpmTasks('grunt-html2js');
+
+    grunt.registerTask('emptyFile', 'Creates an empty file', function() {
+        grunt.file.write('js/views.js', 'angular.module("templates-main", []);');
+    });
+
+
+    // Tell Grunt what to do when we type "grunt" into the terminal
+    grunt.registerTask('default', [
+        'clean',
+        'html2js',
+        'useminPrepare',
+        'copy',
+        'concat',
+        'uglify',
+        'cssmin',
+        'rev',
+        'usemin',
+        'emptyFile'
+    ]);
+
+
+};
+
---
+++ client/js/app.js
@@ -0,0 +1,125 @@
+'use strict';
+
+var app = angular.module('ABC', ['ui.router', 'ui.bootstrap', 'ngAnimate', 'templates-main', 'ngTouch']);
+
+app.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/inicio");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('home', {
+            url: "/inicio",
+            templateUrl: "views/home.html"
+        })
+        .state('place', {
+            url: "/como_llegar",
+            templateUrl: "views/place.html"
+        })
+        .state('hotels', {
+            url: "/donde_dormir",
+            templateUrl: "views/hotels.html"
+        })
+        .state('photos', {
+            url: "/fotos",
+            templateUrl: "views/photos.html",
+            controller: "photosCtrl"
+        })
+        .state('history', {
+            url: "/historia",
+            templateUrl: "views/history.html"
+        });
+}]);
+
+app.run(['$rootScope', function($rootScope){
+
+    $rootScope.$on('$stateChangeStart', function(){
+        $rootScope.collapseNav();
+    });
+
+    $rootScope.collapseNav = function(){
+        $rootScope.isNavCollapsed = true;
+    };
+
+}]);
+
+
+app.controller('photosCtrl',[ '$rootScope','$scope', 'Carousel', function($rootScope,$scope,Carousel){
+    var width = window.innerWidth;
+    var displayMode;
+    if(width >= 750 && width < 1024) displayMode = 'tablet';
+    else if(width < 750) displayMode = 'mobile';
+    else displayMode = 'desktop';
+
+    $scope.carousel = Carousel.create(displayMode);
+}]);
+
+
+app.factory('Carousel', ['Instagram',
+    function(Instagram) {
+        var groupSlides = function(data, slidesPerGroup){
+
+            var i, first = [], second;
+
+            for (i = 0; i < data.length; i += slidesPerGroup) {
+                second = {
+                    image1: data[i]
+                };
+                if (slidesPerGroup == 1) {}
+                if (data[i + 1] && (slidesPerGroup == 2 || slidesPerGroup == 3)) {
+                    second.image2 = data[i + 1];
+
+                }
+                if (data[i + (slidesPerGroup - 1)] && slidesPerGroup == 3) {
+                    second.image3 = data[i + 2];
+                }
+                first.push(second);
+            }
+            return first;
+        };
+
+        return {
+            'create': function(displayMode) {
+
+                var carousel = {
+                    interval: 7000,
+                    hashtag: "wedding",
+                    slidesGroups: []
+                };
+
+                switch(displayMode) {
+                    case 'desktop':
+                        carousel.width = "918px";
+                        carousel.slidesPerGroup = 3;
+                        break;
+                    case 'tablet':
+                        carousel.width = "612px";
+                        carousel.slidesPerGroup = 2;
+                        break;
+                    default:
+                        carousel.width = "303px";
+                        carousel.slidesPerGroup = 1;
+                }
+
+                Instagram.get().success(function(res) {
+                    carousel.slidesGroups = groupSlides(res, carousel.slidesPerGroup);
+                });
+
+                return carousel;
+            }
+        };
+
+    }
+]);
+
+app.factory('Instagram', ['$http',
+    function($http) {
+        return {
+            'get': function() {
+                var request = '/ig_images';
+                return $http.get(request);
+            }
+        };
+    }
+]);
